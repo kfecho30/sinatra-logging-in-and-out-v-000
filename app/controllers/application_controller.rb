841b35ca8f54dc85144c047790fcab03bf66409d
Kelsey Fecho <kfecho30@gmail.com>
2018-04-11 14:21:39 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username], password: params[:password])
-    if !!@user
+    if @user
       session[:user_id] = @user.id
       redirect to '/account'
     else
@@ -22,7 +22,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    erb :account
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
